e23cbd9a16b62bcbf1fed6d8ce89656c635661a4
Lutz Liebig <lutz.liebig@vc-g.de>
2021-06-28 13:58:13 +0200

    rohaufbau projekt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TweetContainerComponent } from './tweet-container/tweet-container.component';
+import { TweetListComponent } from './tweet-list/tweet-list.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { ButtonComponent } from './button/button.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TweetContainerComponent,
+    TweetListComponent,
+    TweetComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tweet-container/tweet-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet-container',
+  templateUrl: './tweet-container.component.html',
+  styleUrls: ['./tweet-container.component.css']
+})
+export class TweetContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
