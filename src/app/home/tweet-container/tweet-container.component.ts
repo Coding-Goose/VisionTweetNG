a4523e2a9f9206f47ce698634a76ad742712d743
Lutz Liebig <lutz.liebig@vc-g.de>
2021-07-25 12:43:52 +0200

    tweet hinzufügen

--- src/app/home/tweet-container/tweet-container.component.ts
+++ src/app/home/tweet-container/tweet-container.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export type tweet = {
+  message: string;
+  user: string;
+  avatarUrl: string;
+};
+
 @Component({
   selector: 'app-tweet-container',
   templateUrl: './tweet-container.component.html',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TweetContainerComponent implements OnInit {
 
-  constructor() { }
+  newTweetText: string = '';
+  tweets: tweet[];
+
+  constructor() {
+    this.tweets =  this.tweets = [
+      {
+        message: "Message Nummer1",
+        user: "User Nummer1",
+        avatarUrl: "https://picsum.photos/100?random=1"
+      },
+      {
+        message: "Message Nummer2",
+        user: "User Nummer2",
+        avatarUrl: "https://picsum.photos/100?random=2"
+      },
+      {
+        message: "Message Nummer3",
+        user: "User Nummer3",
+        avatarUrl: "https://picsum.photos/100?random=3"
+      },
+      {
+        message: "Message Nummer4",
+        user: "User Nummer4",
+        avatarUrl: "https://picsum.photos/100?random=4"
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
 
+  onPost() {
+    this.tweets.push({
+      message: this.newTweetText,
+      user: 'Ich',
+      avatarUrl: "https://picsum.photos/100?random=" + this.tweets.length + 1
+    });
+    this.newTweetText = '';
+  }
 }
--- src/app/home/tweet-list/tweet-list.component.ts
+++ src/app/home/tweet-list/tweet-list.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
-type tweet = {
-  message: string;
-  user: string;
-  avatarUrl: string;
-};
-
+import {Component, Input} from '@angular/core';
+import {tweet} from "../tweet-container/tweet-container.component";
 
 @Component({
   selector: 'app-tweet-list',
@@ -14,33 +8,12 @@ type tweet = {
 })
 export class TweetListComponent {
 
-  tweets: tweet[];
+  @Input() tweets: tweet[] = [];
 
 
   constructor() {
-    this.tweets = [
-      {
-        message: "Message Nummer1",
-        user: "User Nummer1",
-        avatarUrl: "https://picsum.photos/100?random=1"
-      },
-      {
-        message: "Message Nummer2",
-        user: "User Nummer2",
-        avatarUrl: "https://picsum.photos/100?random=2"
-      },
-      {
-        message: "Message Nummer3",
-        user: "User Nummer3",
-        avatarUrl: "https://picsum.photos/100?random=3"
-      },
-      {
-        message: "Message Nummer4",
-        user: "User Nummer4",
-        avatarUrl: "https://picsum.photos/100?random=4"
-      }
-    ];
   }
+
 
   deleteTweet(index: number): void {
     this.tweets.splice(index, 1);
