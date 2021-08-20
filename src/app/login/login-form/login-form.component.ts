17ce8abf580d13a08684b50b07b9bf009336c06e
Lutz Liebig <lutz.liebig@vc-g.de>
2021-08-20 18:18:36 +0200

    LoginForm Validation

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit(): void {
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl("",[Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+      dsgvo: new FormControl(false, [Validators.requiredTrue])
+    });
   }
 
+
+  onLogin() {
+    alert("You just logged in!");
+  }
+
+  onTest() {
+    console.log(this.loginForm);
+  }
 }
